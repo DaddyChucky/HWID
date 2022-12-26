c931e465359b23377cf22dbc19ad79a7a18f02cf
Charles De Lafontaine <46151389+DaddyChucky@users.noreply.github.com>
2022-12-25 19:00:44 -0500

    Initiated colors

---
+++ src/taste_the_rainbow.py
@@ -0,0 +1,90 @@
+# Used as is, see https://github.com/DaddyChucky/UNIVERSAL_SCRAPER for more information about this file.
+
+__title__       = 'taste_the_rainbow.py'
+__doc__         = 'Makes text decoration.'
+__author__      = 'DE LAFONTAINE, Charles.'
+__copyright__   = 'See MIT license description on the GitHub repo. (https://github.com/DaddyChucky/UNIVERSAL_SCRAPER)'
+
+
+###
+#   --> TASTE_THE_RAINBOW <--
+###
+
+
+class TextDecoration:
+    """
+    Interface for text decoration used to output text
+    """
+    HEADER      = '\033[95m'
+    OKBLUE      = '\033[94m'
+    OKCYAN      = '\033[96m'
+    OKGREEN     = '\033[92m'
+    WARNING     = '\033[93m'
+    FAIL        = '\033[91m'
+    ENDC        = '\033[0m'
+    BOLD        = '\033[1m'
+    UNDERLINE   = '\033[4m'
+
+
+def print_header(category: str, text_to_print: str) -> None:
+    """
+    Prints text as header
+
+    Args:
+        category        (str): Message category
+        text_to_print   (str): Text to print (output)
+            
+    Returns:
+        None
+    """
+    print(
+        TextDecoration.HEADER + TextDecoration.BOLD + "[" + category + "] > " + TextDecoration.ENDC + TextDecoration.OKBLUE +
+        text_to_print + TextDecoration.ENDC)
+
+
+def print_success(category: str, text_to_print: str) -> None:
+    """
+    Prints text as success
+
+    Args:
+        category        (str): Message category
+        text_to_print   (str): Text to print (output)
+            
+    Returns:
+        None
+    """
+    print(
+        TextDecoration.HEADER + TextDecoration.BOLD + "[" + category + "] > " + TextDecoration.ENDC + TextDecoration.OKGREEN +
+        text_to_print + " ✓ " + TextDecoration.ENDC)
+
+
+def print_warning(category: str, text_to_print: str) -> None:
+    """
+    Prints text as warning
+
+    Args:
+        category        (str): Message category
+        text_to_print   (str): Text to print (output)
+            
+    Returns:
+        None
+    """
+    print(
+        TextDecoration.HEADER + TextDecoration.BOLD + "[" + category + "] > " + TextDecoration.ENDC + TextDecoration.WARNING +
+        text_to_print + " ⚠ " + TextDecoration.ENDC)
+
+
+def print_failure(category: str, text_to_print: str) -> None:
+    """
+    Prints text as failure
+
+    Args:
+        category        (str): Message category
+        text_to_print   (str): Text to print (output)
+            
+    Returns:
+        None
+    """
+    print(
+        TextDecoration.HEADER + TextDecoration.BOLD + "[" + category + "] > " + TextDecoration.ENDC + TextDecoration.FAIL +
+        text_to_print + " ✗ " + TextDecoration.ENDC)
